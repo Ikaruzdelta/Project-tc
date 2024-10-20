ee547eaad89a866146775bf770fe1b01417c5736
Ikaruz <ikaruz.music@hotmail.com>
2024-10-19 21:20:48 -0300

    update

--- backend/src/main/java/com/example/projecttc/config/WebConfig.java
+++ backend/src/main/java/com/example/projecttc/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+   public WebConfig() {
+   }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  
-            .allowedOrigins("http://127.0.0.1:5500") 
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
+   public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+      .allowedOrigins(new String[]{"*"})
+      .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+      .allowedHeaders(new String[]{"*"});
+   }
 }
